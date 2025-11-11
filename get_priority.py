440bf9f17a87f9de9d8696fd62b6fe609c43d98a
Yuping Fan <fany@Mac.ht.home>
2025-11-11 15:42:22 -0600

    save priority as int

--- get_priority.py
+++ get_priority.py
@@ -7,14 +7,19 @@ output_file = "alpaca_requests_1000_priority.jsonl"
 
 with open(input_file, "r") as fin, open(output_file, "w") as fout:
     for i, line in enumerate(fin, start=1):
-        prompt = line.strip()
-        obj = json.loads(line)
-        if not prompt:
+        if not line.strip():
             continue  # skip empty lines
+        obj = json.loads(line)
+        prompt = obj["prompt"]
+        
 
-        print(f"Request {i}: {prompt}")
-
-        response = requests.post(url, json={"prompt": obj["prompt"]})
-        fout.write(f"{response.text}\n")
+        response = requests.post(url, json={"prompt": prompt})
+        # fout.write(f"{response.text}\n")
+        priority = int(float(json.loads(response.text)["priority"]) * 1000000)
+        record = {
+            "prompt": prompt,
+            "priority": priority
+        }
+        fout.write(json.dumps(record) + "\n")
 
 print(f"✅ All responses saved to {output_file}")
